5a3d985569dfc8b757a12e9a883a1562820f5202
Biekei Ojo <dumb906@gmail.com>
2011-12-15 10:33:23 +0100

    added class HtmlTable to create a table from iterables added an incomplete sidebar generation function adjustments to the functions implementing queries

--- functions.py
+++ functions.py
@@ -1,11 +1,15 @@
 import hashlib, random, json
+from lxml import etree
+
+etree.El = etree.Element
+etree.SubEl = etree.SubElement
 
 from django.http import HttpResponse, Http404
 from django.contrib.sessions.models import Session
 from django.template import loader, RequestContext, Template
 from django.utils.datastructures import SortedDict
 
-from tiote import models
+from tiote import sql
 
 ajaxKey = ''
         
@@ -17,21 +21,37 @@ def rpr_query(request, query_type, query_data=None):
     
     if query_type in no_return_queries:
         conn_params['database'] = request.GET.get('database') if request.GET.get('database') else conn_params['database']
-        q = models.generate_query( query_type, dialect=conn_params['dialect'],
-                query_data=query_data)
-        result = models.short_query(conn_params, q)
+        q = sql.generate_query( query_type, conn_params['dialect'],query_data)
+        result = sql.short_query(conn_params, q)
         return HttpResponse( json.dumps(result) )
     
     # specific queries with implementations similar to both dialects
     elif query_type == 'user_rpr':
         if conn_params['dialect'] == 'mysql':
             conn_params['database'] = 'mysql'
-        r = models.full_query(conn_params, 
-            models.stored_query(request.GET.get('query'),conn_params['dialect']) )
+        r = sql.full_query(conn_params, 
+            sql.stored_query(request.GET.get('query'),conn_params['dialect']) )
         if type(r) == dict:
             return jsonize_result(r)
         else:
             return http_500(r)
+        
+        
+    elif query_type in ['table_keys', 'primary_keys']:
+        if query_data is None:
+            query_data = {'database':request.GET.get('database'),'table':request.GET.get('table')}
+            if request.GET.get('schema'):
+                query_data['schema'] = request.GET.get('schema')
+        
+        if conn_params['dialect'] == 'postgresql': conn_params['database'] = query_data['database']
+        r = []
+        for q in sql.generate_query(query_type, conn_params['dialect'], query_data):
+            r.append(sql.full_query(conn_params, q) )
+        # format postgresql
+        
+        return r[0]
+        
+    
     
     elif query_type in ['table_rpr', 'table_structure']:
         conn_params['database'] = request.GET.get('database')
@@ -40,9 +60,26 @@ def rpr_query(request, query_type, query_data=None):
             sub_q_data['table'] = request.GET.get('table')
         if request.GET.get('schema'):
             sub_q_data['schema'] = request.GET.get('schema')
-            
-        r = models.full_query(conn_params,
-            models.generate_query(query_type, conn_params['dialect'], sub_q_data)[0] )
+        # make query
+        r = sql.full_query(conn_params,
+            sql.generate_query(query_type, conn_params['dialect'], sub_q_data)[0] )
+        # further needed processing
+        if conn_params['dialect'] == 'postgresql' and query_type =='table_structure':
+            rwz = []
+            for tuple_row in r['rows']:
+                row = list(tuple_row)
+                if row[1] in ['bit', 'bit varying', 'character varying', 'character'] and type(row[4]) is int:
+                    row[1] += '({0})'.format(row[4])
+                elif row[1] in ['numeric', 'decimal'] and type(row[5]) is int or type(row[6]) is int:
+                    row[1] += '({0},{1})'.format(row[5], row[6])
+                elif row[1] in ['interval', 'time with time zone', 'time without time zone',
+                    'timestamp with time zone', 'timestamp without time zone'] and type(row[7]) is int:
+                    row[1] += '({0})'.format(row[7])
+                # append the current row to rwz
+                rwz.append([row[0], row[1], row[2], row[3] ])
+            # change r['rows']
+            r['rows'] = rwz
+            r['columns'] = [ r['columns'][0], r['columns'][1], r['columns'][2], r['columns'][3] ]
         if type(r) == dict:
             return jsonize_result(r)
         else:
@@ -57,14 +94,12 @@ def rpr_query(request, query_type, query_data=None):
             sub_q_data['schema'] = request.GET.get('schema')
         # retrieve and run queries
         conn_params['database'] = request.GET.get('database')
-        q = models.generate_query('table_keys', conn_params['dialect'], sub_q_data)[0]
-        keys = models.full_query(conn_params,
-            q )
-        count = models.full_query(conn_params, 
-            models.generate_query('count_rows', conn_params['dialect'], sub_q_data)[0],
-            )['rows'][0][0]
-        r = models.full_query(conn_params,
-            models.generate_query(query_type, conn_params['dialect'], sub_q_data)[0]
+        keys = rpr_query(request, 'primary_keys', sub_q_data)
+        count = sql.full_query(conn_params, 
+            sql.generate_query('count_rows', conn_params['dialect'], sub_q_data)[0],
+            )
+        r = sql.full_query(conn_params,
+            sql.generate_query(query_type, conn_params['dialect'], sub_q_data)[0]
             )
         # format and return data
         if type(r) == dict:
@@ -74,6 +109,19 @@ def rpr_query(request, query_type, query_data=None):
         else:
             return http_500(r)
         
+    # queries that just asks formats and return result
+    elif query_type in ['existing_columns', 'existing_tables', 'table_with_columns']:
+        query_data = {'database':request.GET.get('database'),'table':request.GET.get('table')}
+        if conn_params['dialect'] == 'postgresql':
+            query_data['schema'] = request.GET.get('schema')
+            conn_params['database'] = query_data['database']
+            
+        q = sql.generate_query(query_type, conn_params['dialect'], query_data)
+        r =  sql.full_query(conn_params,
+            q[0])
+        return r['rows']
+
+        
     # queries with dissimilar implementations
     elif conn_params['dialect'] == 'postgresql':
         
@@ -81,14 +129,9 @@ def rpr_query(request, query_type, query_data=None):
             # change default database
             if 'db' in query_data:
                 conn_params['database'] = query_data['db']
-            return models.full_query(conn_params,
-                models.stored_query(query_type, conn_params['dialect']))
+            return sql.full_query(conn_params,
+                sql.stored_query(query_type, conn_params['dialect']))
         
-        elif query_type == 'existing_tables':
-            conn_params['database'] = request.GET['database']
-            return models.full_query(conn_params,
-                models.stored_query(query_type, conn_params['dialect']))['rows']
-
         else:
             return http_500('query not implemented!')
             
@@ -97,14 +140,9 @@ def rpr_query(request, query_type, query_data=None):
         
         if query_type == 'describe_databases':
             conn_params['database'] = 'INFORMATION_SCHEMA';
-            query = models.stored_query(query_type, conn_params['dialect'])
-            return models.full_query(conn_params, query)
+            query = sql.stored_query(query_type, conn_params['dialect'])
+            return sql.full_query(conn_params, query)
         
-        elif query_type == 'existing_tables':
-            query_data = {'database':request.GET.get('database')}
-            return models.full_query(conn_params,
-                models.generate_query(query_type, conn_params['dialect'], query_data)[0])['rows']
-
         else:
             return http_500('query not yet implemented!')
     else:
@@ -113,33 +151,20 @@ def rpr_query(request, query_type, query_data=None):
 
 def common_query(request, query_name):
     conn_params = get_conn_params(request)
-    pgsql_redundant_queries = ['template_list', 'group_list', 'user_list', 'db_list']
+    pgsql_redundant_queries = ['template_list', 'group_list', 'user_list', 'db_list', 'schema_list']
     mysql_redundant_queries = ['db_list','charset_list', 'supported_engines']
     
-    if conn_params['dialect'] == 'postgresql':
-        if query_name == 'describe_databases':
-            db_list = models.full_query(conn_params, 
-                models.stored_query('db_list', conn_params['dialect']) )
-            dict_db = SortedDict()
-            for db in db_list['rows']:
-                r = rpr_query(request, 'table_list', {'db':db[0]})['rows']
-                d = SortedDict()
-                for tup in r:
-                    if tup[0] not in d:
-                        d[ tup[0] ] = []
-                    d[ tup[0] ].append((tup[1],))
-                dict_db[ db[0] ] = d
-            return json.dumps(dict_db)
-        
-        elif query_name in pgsql_redundant_queries :
+    if conn_params['dialect'] == 'postgresql' and query_name in pgsql_redundant_queries :
             # this kind of queries require no special attention
-            return models.full_query(conn_params,
-                models.stored_query(query_name, conn_params['dialect']))['rows']
+            conn_params['database'] == request.GET.get('database') if request.GET.get('database') else conn_params['database']
+            r = sql.full_query(conn_params,
+                sql.stored_query(query_name, conn_params['dialect']))
+            return r['rows']
                 
     elif conn_params['dialect'] == 'mysql':
         
         if query_name == 'db_names':
-            db_names =  models.get_databases( get_conn_params(request) )
+            db_names =  sql.get_databases( get_conn_params(request) )
             return result_to_json(db_names)
         
         elif query_name == 'describe_databases':
@@ -153,11 +178,16 @@ def common_query(request, query_name):
         
         elif query_name in mysql_redundant_queries :
             # this kind of queries require no special attention
-            return models.full_query(conn_params,
-                models.stored_query(query_name, conn_params['dialect']))['rows']
+            return sql.full_query(conn_params,
+                sql.stored_query(query_name, conn_params['dialect']))['rows']
         
         
-        
+
+def table_with_count(request,):
+    pass
+
+
+
 # returns page templates for each view
 def skeleton(which, section=None ):
     s = section+'/' if section != None else ''
@@ -176,7 +206,7 @@ def do_login(request, cleaned_data):
     dict_post = {'username':username,'password':password,'database_driver':database_driver, 'host':host}
     if 'connection_database' in cleaned_data:
         dict_post['connection_database'] = cleaned_data['connection_database']
-    dict_cd = models.model_login(dict_post)
+    dict_cd = sql.model_login(dict_post)
     if not dict_cd['login']:
         #authentication failed
         return dict_cd
@@ -221,7 +251,7 @@ def validateAjaxRequest(request):
         
 def inside_query(request, query_name):
     if query_name == 'db_names':
-        db_names =  models.get_databases( get_conn_params(request) )
+        db_names =  sql.get_databases( get_conn_params(request) )
         return db_names.fetchall()
     
 def result_to_json(result):
@@ -242,7 +272,7 @@ def get_home_variables(request):
     p = get_conn_params(request)
     variables = {'user': p['username'], 'host': p['host']}
     variables['dialect'] = 'PostgreSQL' if p['dialect'] == 'postgresql' else 'MySQL'
-    result = models.full_query( p, models.stored_query('variables', p['dialect']))
+    result = sql.full_query( p, sql.stored_query('variables', p['dialect']))
     if p['dialect'] == 'postgresql':
         variables['version'] = result['rows'][0]
         return variables
@@ -333,3 +363,122 @@ def response_shortcut(request, template = False, extra_vars=False ):
     if template == 'form_errors':
         h.set_cookie('tt_formContainsErrors','true')
     return h
+
+
+def generate_sidebar(request):
+
+    def select_input(rows, desc=None, initial=None):
+        # build select's El attributes
+        attrib_str = ''
+        for k in desc.keys():
+            attrib_str += " " + k +"='" + desc[k] +"'"
+        # build select's options
+        options_str = ''
+        for row in rows:
+            prefx = '' 
+            if initial is not None and initial == row[0]:
+                prefx = " selected='selected'"
+            options_str += "<option value='{0}'{1}>{0}</option>".format(
+                    row[0], prefx
+                )
+        # final select string
+        return "<select{0}>{1}</select>".format(attrib_str, options_str)
+
+    ret_string = ''
+    
+    conn_params = get_conn_params(request)
+    db_list = common_query(request, 'db_list')
+#    # common links
+#    p = etree.fromstring("<div><h6>Quick Links</h6><ul></ul></div>")
+#    for r in ['home','query']:
+#        li = etree.SubEl(p.getchildren()[1], 'li')
+#        etree.SubEl(li, 'a', attrib={'href':'#section=home&view=' + r}).text = r
+#    ret_string += etree.tostring(p, method='html')
+#    
+    s = ''
+    if request.GET.get('section') == 'home':
+        for db_row in db_list:
+            sufx = "&schema=public" if conn_params['dialect'] == 'postgresql' else ''
+            s += "<li><a class='icon-database' href='#section=database&view=overview&database=" +db_row[0]+sufx+"'>" + db_row[0] + "</a></li>"
+        ret_string += '<h6>Databases</h6><ul>' + s + '</ul>'
+    
+    elif request.GET.get('database'):
+        d = {'database': request.GET.get('database')}
+        db_selection_form = select_input(db_list, desc={'id':'db_select'}, initial=d['database'])
+        if conn_params['dialect'] == 'postgresql':
+            d['schema'] = request.GET.get('schema')
+            # append schema selection with default to public
+            schema_list = common_query(request, 'schema_list')
+            schema_selection_form = select_input(schema_list,desc={'id':'schema_select'},initial=d['schema'])
+            s += "<h6 class='icon-schemas'>schema</h6>" + schema_selection_form
+        
+        # table selection ul
+        table_list = rpr_query(request, 'existing_tables')
+        sfx_list = []
+        pg_sfx = '&schema=' + d['schema'] if conn_params['dialect']=='postgresql' else ''
+        for tbl_row in table_list:
+            sfx_list.append(
+                "<li><a class='icon-table' href='#section=table&view=browse&database={0}{1}&table={2}'>{2}</a></li>".format(
+                    d['database'], pg_sfx, tbl_row[0]
+                )
+            )
+        sfx = "<ul>{0}</ul>".format( ''.join(sfx_list) )
+        ret_string += "<h6 class='icon-databases'>databases</h6>"+ db_selection_form + s + sfx
+    return ret_string
+
+
+
+class HtmlTable():
+    '''
+    creates a html table from the given arguments
+        - properties - a dict of table attributes
+        - headers - an iterable containing the table heads
+        - rows - and iterable containing some iterables
+    '''
+    def __init__(self, headers=None, rows=None, attribs=None, props=None):
+        self.props = props
+        self.tbody_chldrn = []
+        self.attribs = attribs
+        # build attributes
+        self.attribs_list = self.build_attribs_list(attribs)
+        # build <thead><tr> children
+        if headers is not None:
+            hd_list = []
+            if self.props is not None and self.props['with_checkboxes'] == True:
+                hd_list.append("<th class='selector'></th>")
+            for head in headers:
+                hd_list.append('<th>'+head+'</th>')
+            self.thead_chldrn = hd_list
+        # build <tbody> children
+        if rows is not None:
+            for row in rows:
+                self.push(row)
+        
+    def build_attribs_list(self, attribs=None):
+        attribs_list = []
+        if attribs is not None:
+            for k in attribs.keys():
+                attribs_list.append(" {0}='{1}'".format(
+                    str(k).lower(), str(attribs[k]) )
+                )
+        return attribs_list
+
+    def push(self, row, props=None):
+        count = len(self.tbody_chldrn)
+        row_list = ["<tr id='row_{0}'>".format(str(count))]
+        if self.props is not None and self.props['with_checkboxes'] == True:
+            anc_chk = "<input class='checker' id='check_{0} type='checkbox' />".format(count)
+            tida = "<td class='selector'>{0}</td>".format(anc_chk)
+            row_list.append(tida)
+        for col in row:
+            row_list.append("<td>{0}</td>".format(col))
+        row_list.append("</tr>")
+        self.tbody_chldrn.append(row_list)
+    
+    def to_element(self):
+        el = "<table{0}><thead><tr>{1}</tr></thead><tbody>{2}</tbody></table>".format(
+            ''.join(self.attribs_list), ''.join(self.thead_chldrn), 
+            ''.join([ ''.join(row) for row in self.tbody_chldrn])
+        )
+        return el
+    
